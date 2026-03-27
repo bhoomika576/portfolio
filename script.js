0d91c5e02588102c60d51aff1abc2eabff7aa11d
bhoomika576 <bhoomikamudhoo@gmail.com>
2026-03-27 07:28:06 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const text = "Bhoomika 👋";
+let index = 0;
+
+function typeEffect() {
+    if (index < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 100);
+    }
+}
+
+window.onload = typeEffect;
