2f7e5607e3b2e83b74e276440d65498e53f9b1ad
bhoomika576 <bhoomikamudhoo@gmail.com>
2026-03-28 18:11:30 +0400

    S

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-const text = "Bhoomika ";
-let index = 0;
+   
+        //const text = "Bhoomika";
+        //let index = 0;
+        //function typeEffect() {
+        //    if (index < text.length) {
+        //        document.getElementById("typing").innerHTML += text.charAt(index);
+        //        index++;
+        //        setTimeout(typeEffect, 100);
+            //}
+        //}
+     
 
-function typeEffect() {
-    if (index < text.length) {
-        document.getElementById("typing").innerHTML += text.charAt(index);
-        index++;
-        setTimeout(typeEffect, 100);
-    }
-}
-window.onload = typeEffect;
+    const typed = new Typed('#typing', {
+        strings: ['Bhoomika', 'a Developer', 'a Problem Solver', 'a Tech Enthusiast'],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true
+    });
+
+    const menuBtn = document.getElementById('menu-btn');
+    const nav = document.getElementById('nav');
+
+    menuBtn.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+
+
+    
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener("click", function(e) {
+            const target = this.getAttribute("href");
+            if (target === "#") return;
+            const element = document.querySelector(target);
+            if (element) {
+                e.preventDefault();
+                element.scrollIntoView({ behavior: "smooth" });
+            }
+        });
+    });
+    
+    window.onload = typeEffect;
